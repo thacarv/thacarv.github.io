43edcae0c3482f6d56524e0e5f8b0e3e2d8f11b8
Monosune <thiagoassisdecarvalho@gmail.com>
2025-05-23 18:18:39 -0300

    more updates

--- script/main.js
+++ script/main.js
@@ -9,3 +9,23 @@ function clicktocontinue() {
 }
 
 document.addEventListener("click", clicktocontinue);
+
+// Checks if the document is loaded.
+document.addEventListener("DOMContentLoaded", () => {
+  // Uses IntersectionObserver API to check if objects are on screen.
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+        return;
+      }
+    });
+  });
+
+  // Inside HTML you can put a specific class to mark the element to the API.
+  // API checks every element that contains the animate class.
+  const allAnimatedElements = document.querySelectorAll(".animate");
+
+  // Adds the observer to each element with animate class.
+  allAnimatedElements.forEach((element) => observer.observe(element));
+});
